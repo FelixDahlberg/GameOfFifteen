3343ad9f6a644478a3affceeb8d1c094dac88a2a
JohanJohnsson <johan.johnsson@yh.nackademin.se>
2023-10-27 08:53:17 +0200

    Flyttat/tagit bort knappar

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,27 @@ public class Main extends JFrame implements ActionListener {
     public static void main(String[] args) {
         new Main();
     }
-    
+
     northPanel northPanel = new northPanel();
-    JPanel centerPanel = new JPanel(new GridLayout(4,4));
+    JPanel centerPanel = new JPanel(new GridLayout(4, 4));
     southPanel southPanel = new southPanel();
-    
-    
-    
-    Main(){
+
+
+    Main() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         setLayout(new BorderLayout());
         add(northPanel, BorderLayout.NORTH);
         add(centerPanel, BorderLayout.CENTER);
         add(southPanel, BorderLayout.SOUTH);
 
         northPanel.newGameButton.addActionListener(this);
-        
-        
+
+
         pack();
         setLocationRelativeTo(null);
         setResizable(false);
@@ -32,12 +37,12 @@ public class Main extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == northPanel.newGameButton){
-            southPanel.moveCounter ++;
+        if (e.getSource() == northPanel.newGameButton) {
+            southPanel.moveCounter++;
             southPanel.moveCounterPanel.setText(String.valueOf(southPanel.moveCounter));
             if (southPanel.timer.isRunning()) {
                 southPanel.timer.stop();
-                
+
             } else {
                 southPanel.running = true;
                 southPanel.timer.start();
